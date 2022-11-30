1ce344fa4a335a663dae32b8fd87367e07f390d0
Szász Tamás <tamas.szasz12@gmail.com>
2022-11-30 21:35:55 +0200

    Add project files.

---
+++ Areas/Identity/AdditionalUserClaimsPrincipalFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CAFFEINE.Areas.Identity
+{
+    public class AdditionalUserClaimsPrincipalFactory :
+        UserClaimsPrincipalFactory<IdentityUser>
+    {
+        public AdditionalUserClaimsPrincipalFactory(
+            UserManager<IdentityUser> userManager,
+            IOptions<IdentityOptions> optionsAccessor)
+            : base(userManager, optionsAccessor)
+        {
+        }
+
+        public async override Task<ClaimsPrincipal> CreateAsync(IdentityUser user)
+        {
+            var principal = await base.CreateAsync(user);
+            var identity = (ClaimsIdentity)principal.Identity;
+
+            var claims = new List<Claim>();
+
+            if (user.TwoFactorEnabled)
+            {
+                claims.Add(new Claim("amr", "mfa"));
+            }
+            else
+            {
+                claims.Add(new Claim("amr", "pwd"));
+            }
+
+            identity.AddClaims(claims);
+            return principal;
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CAFFEINE.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CAFFEINE.Areas.Identity.IdentityHostingStartup))]
+namespace CAFFEINE.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
