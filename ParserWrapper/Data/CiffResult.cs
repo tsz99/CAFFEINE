c929703880049a66a504dbfe84a4dbedfb5d15e2
Dávid Dorián <david.dorian99@gmail.com>
2022-12-01 23:06:20 +0100

    added wrapper for parser

---
+++ ParserWrapper/Data/CiffResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing
+{
+    public class CiffResult
+    {
+        public List<string> Tags { get; set; }
+        public string Caption { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Duration { get; set; }
+        public string ImageName { get; set; }
+    }
+}
